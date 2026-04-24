60a5b860dac263e3a4f5a39eca2460316f8149f4
ki1rei <studyacctyara@gmail.com>
2026-04-24 21:35:38 +0700

    adding konversiDesimalKeBiner

--- StackTugas Mahasiswa13.java
+++ StackTugas Mahasiswa13.java
@@ -75,4 +75,18 @@ class StackTugasMahasiswa13 {
     public int jumlahTugas() {
         return top + 1;
     }
+
+    public String konversiDesimalKeBiner(int nilai) {
+        StackKonversi13 stack = new StackKonversi13();
+        while (nilai > 0) {
+            int sisa = nilai % 2;
+            stack.push(sisa);
+            nilai = nilai / 2;
+        }
+        String biner = new String();
+        while (!stack.isEmpty()) {
+            biner += stack.pop();
+        }
+        return biner;
+    }
 }
